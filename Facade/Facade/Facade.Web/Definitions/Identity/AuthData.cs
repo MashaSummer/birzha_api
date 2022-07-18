2281329e2d25f55dedfa0468259eed5742c936db
Artem <leoartemlev@yandex.ru>
2022-07-18 14:28:49 +0500

    feat: add facade template

---
+++ Facade/Facade/Facade.Web/Definitions/Identity/AuthData.cs
@@ -0,0 +1,8 @@
+﻿using OpenIddict.Server.AspNetCore;
+
+namespace Facade.Web.Definitions.Identity;
+
+public static class AuthData
+{
+    public const string AuthSchemes = OpenIddictServerAspNetCoreDefaults.AuthenticationScheme;
+}
