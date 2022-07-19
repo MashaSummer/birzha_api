6c72ca923a5cd686afeb5515d6daa55b42d69fab
Artem <leoartemlev@yandex.ru>
2022-07-19 14:01:10 +0500

    save changes

---
+++ Facade/Facade/Facade.Web/Auth/TestService.cs
@@ -0,0 +1,18 @@
+﻿using AuthRequest;
+using Grpc.Core;
+
+namespace Facade.Web.Auth;
+
+public class TestService : AuthService.AuthServiceBase
+{
+    private readonly ILogger<TestService> _logger;
+
+    public TestService(ILogger<TestService> logger) => _logger = logger;
+
+    public override Task<TokenData> Login(LoginData request, ServerCallContext context)
+    {
+        _logger.Log(LogLevel.Information, $"Login: {request.Email} Password: {request.Password}");
+
+        return Task.FromResult(new TokenData() { Token = "To123456789ken" });
+    }
+}
