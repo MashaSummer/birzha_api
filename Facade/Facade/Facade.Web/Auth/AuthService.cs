38e480a7f48db76e89046525ed1078cf4bddd813
Artem <leoartemlev@yandex.ru>
2022-07-19 09:58:00 +0500

    last changes

---
+++ Facade/Facade/Facade.Web/Auth/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using AuthRequest;
+using Grpc.Core;
+
+namespace Facade.Web.Auth;
+
+public class AuthService : AuthRequest.AuthService.AuthServiceBase
+{
+    private readonly ILogger<AuthService> _logger;
+
+    public AuthService(ILogger<AuthService> logger) => _logger = logger;
+
+    
+    public override Task<TokenData> Login(LoginData request, ServerCallContext context)
+    {
+        _logger.LogInformation($"Got login request: {request}");
+        return Task.FromResult(new TokenData() { Token = "To12345678ken" });
+    }
+}
