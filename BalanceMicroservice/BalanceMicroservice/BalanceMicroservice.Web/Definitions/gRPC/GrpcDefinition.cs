ea96bfb99e4e674eb6e244ab20f85010db31f807
UndeadNito <113023091@mail.ru>
2022-07-20 13:46:47 +0600

    get balance via grpc

---
+++ BalanceMicroservice/BalanceMicroservice/BalanceMicroservice.Web/Definitions/gRPC/GrpcDefinition.cs
@@ -0,0 +1,21 @@
+﻿using BalanceMicroservice.Web.Definitions.Base;
+using BalanceMicroservice.Web.Endpoints.BalanceEndpoints;
+using BalanceMicroservice.Web.Endpoints.BalanceEndpoints.ViewModels;
+
+namespace BalanceMicroservice.Web.Definitions.Mongo
+{
+    public class GrpcDefinition : AppDefinition
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddGrpc();
+        }
+
+        public override void ConfigureApplication(WebApplication app, IWebHostEnvironment env)
+        {
+            app.UseEndpoints(endpoints =>
+                endpoints.MapGrpcService<GetBalanceController>()
+                );
+        }
+    }
+}
---
+++ BalanceMicroservice/BalanceMicroservice/BalanceMicroservice.Web/Endpoints/BalanceEndpoints/GetBalanceControrller.cs
@@ -0,0 +1,19 @@
+﻿namespace BalanceMicroservice.Web.Endpoints.BalanceEndpoints
+{
+    public class GetBalanceController: GetBalanceService.GetBalanceServiceBase
+    {
+        private readonly BalanceService _database;
+        public GetBalanceController(BalanceService mongo)
+        {
+            _database = mongo;
+        }
+
+        public GetBalanceResponse GetBalance(GetBalanceRequest request)
+        {
+            return new GetBalanceResponse
+            {
+                Balance = _database.GetAsync(new Guid(request.Id)).Result.Balance
+            };
+        }
+    }
+}
