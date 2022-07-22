75a2fc05e5cd4f2f3a8a35c498162c8a700807f7
Artem <leoartemlev@yandex.ru>
2022-07-22 17:41:30 +0500

    feat: add test endpoint and json with test data for import to database

---
+++ ProductMicroservice/ProductMicroservice/ProductMicroservice.Web/Endpoints/MongoTestEndpoint/MongoTestEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProductMicroservice.Web.Definitions.Base;
+using ProductMicroservice.Web.Endpoints.MongoTestEndpoint.Queries;
+using ProductMicroservice.Web.Endpoints.MongoTestEndpoint.ViewModels;
+
+namespace ProductMicroservice.Web.Endpoints.MongoTestEndpoint;
+
+public class MongoTestEndpoint : AppDefinition
+{
+    public override void ConfigureApplication(WebApplication app, IWebHostEnvironment env) => 
+        app.MapGet("/get/records/all", GetAllRecords);
+
+    private async Task<ProductViewModel[]> GetAllRecords([FromServices] IMediator mediator, HttpContext context) => 
+        await mediator.Send(new AllRecordsRequest(), context.RequestAborted);
+}
---
+++ ProductMicroservice/ProductMicroservice/ProductMicroservice.Web/Endpoints/MongoTestEndpoint/ViewModels/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProductMicroservice.Web.Endpoints.MongoTestEndpoint.ViewModels;
+
+public class ProductViewModel
+{
+    [BsonId]
+    [BsonIgnoreIfNull]
+    public string Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; } = null!;
+    
+    [BsonElement("best_ask")]
+    public double? BestAsk { get; set; }
+    
+    [BsonElement("best_bid")]
+    public double? BestBid { get; set; }
+
+}
