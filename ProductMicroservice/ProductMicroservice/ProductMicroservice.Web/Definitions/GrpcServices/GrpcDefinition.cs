6f35f457aa4a850e2c4c9ce92459b1fc02041846
Artem <leoartemlev@yandex.ru>
2022-07-26 16:12:05 +0500

    feat: add grpc reflection for testing in grpccurl

--- ProductMicroservice/ProductMicroservice/ProductMicroservice.Web/Definitions/GrpcServices/GrpcDefinition.cs
+++ ProductMicroservice/ProductMicroservice/ProductMicroservice.Web/Definitions/GrpcServices/GrpcDefinition.cs
@@ -7,11 +7,15 @@ public class GrpcDefinition : AppDefinition
     public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddGrpc();
+        services.AddGrpcReflection();
+
     }
 
     public override void ConfigureApplication(WebApplication app, IWebHostEnvironment env)
     {
         app.UseRouting();
         app.UseEndpoints(endpoint => endpoint.MapGrpcService<ProductService>());
+
+        app.MapGrpcReflectionService();
     }
 }
