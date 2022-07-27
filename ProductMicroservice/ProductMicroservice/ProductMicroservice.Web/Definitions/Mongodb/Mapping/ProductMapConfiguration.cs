f25ef20992728a9fadddc7c60276468eb7cad0ba
Artem Levin <leoartemlev@yandex.ru>
2022-07-27 22:03:43 +0500

    feat: simplify mapping from ProductModel to grpc Product object more

--- ProductMicroservice/ProductMicroservice/ProductMicroservice.Web/Definitions/Mongodb/Mapping/ProductMapConfiguration.cs
+++ ProductMicroservice/ProductMicroservice/ProductMicroservice.Web/Definitions/Mongodb/Mapping/ProductMapConfiguration.cs
@@ -8,14 +8,6 @@ public class ProductMapConfiguration : Profile
 {
     public ProductMapConfiguration()
     {
-        CreateMap<ProductModel, ProductArray.Types.Product>()
-            .ForMember(d => d.Id, 
-                s => s.MapFrom(x => x.Id))
-            .ForMember(d => d.Name, 
-                s => s.MapFrom(x => x.Name))
-            .ForMember(d => d.BestAsk, 
-                s => s.MapFrom(x => x.BestAsk))
-            .ForMember(d => d.BestBid, 
-                s => s.MapFrom(x => x.BestBid));
+        CreateMap<ProductModel, ProductArray.Types.Product>();
     }
 }
