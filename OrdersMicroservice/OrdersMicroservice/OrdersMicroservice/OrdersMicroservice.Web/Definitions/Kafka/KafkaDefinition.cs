f2d3f461f57fce9e884695981c7555b669181d92
Artem Levin <leoartemlev@yandex.ru>
2022-08-11 15:14:07 +0500

    feat: change proto for sending to Orders_Candidates topic and remove unnecessary files

--- OrdersMicroservice/OrdersMicroservice/OrdersMicroservice/OrdersMicroservice.Web/Definitions/Kafka/KafkaDefinition.cs
+++ OrdersMicroservice/OrdersMicroservice/OrdersMicroservice/OrdersMicroservice.Web/Definitions/Kafka/KafkaDefinition.cs
@@ -1,5 +1,4 @@
 using Confluent.Kafka;
-using GrpcServices;
 using OrdersEvent;
 using OrdersMicroservice.Definitions.Base;
 using OrdersMicroservice.Definitions.Kafka.Handlers;
@@ -13,7 +12,7 @@ public class KafkaDefinition : AppDefinition
 {
     public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        
+
         var isEnableKafka = bool.Parse(configuration["Kafka:IsEnable"]);
         if (!isEnableKafka)
             return;
@@ -22,44 +21,19 @@ public class KafkaDefinition : AppDefinition
         var ordersCreatedConfig = configuration.GetSection("Kafka:OrdersCreatedProducer").Get<KafkaProducerConfig>();
         services.AddKafkaProducer<Null, OrderCreatedEvent>(ordersCreatedConfig);
 
-        
+
         // For "Orders_Executed" topic
         var ordersExecutedConfig = configuration.GetSection("Kafka:OrdersExecutedProducer").Get<KafkaProducerConfig>();
         services.AddKafkaProducer<Null, OrderExecuteEvent>(ordersExecutedConfig);
-        
+
         // For "Orders_Candidates" topic
         var ordersCandidatesConfig =
             configuration.GetSection("Kafka:OrdersCandidatesProducer").Get<KafkaProducerConfig>();
         services.AddKafkaProducer<Null, CandidatesFoundEvent>(ordersCandidatesConfig);
-        
+
         // For "Orders_Validation" topic
         var ordersValidationConfig =
             configuration.GetSection("Kafka:OrdersValidationConsumer").Get<KafkaConsumerConfig>();
         services.AddKafkaConsumer<Null, OrderValidationEvent>(ordersValidationConfig);
-        
-
-    }
-
-
-    public override void ConfigureApplication(WebApplication app, IWebHostEnvironment env)
-    {
-        app.MapPost("/kafka/test", async context =>
-        {
-            var request = new Request()
-            {
-                Name = "Some name"
-            };
-
-            var producer = app.Services.GetRequiredService<IEventProducer<Null, Request>>();
-
-            var result = await producer.ProduceAsync(null, request);
-
-            if (result.Ok)
-                Results.Ok();
-            else
-            {
-                Results.BadRequest();
-            }
-        });
     }
 }
