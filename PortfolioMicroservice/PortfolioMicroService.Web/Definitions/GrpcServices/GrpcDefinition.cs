0ac771634e339e098721fcd5de1e182c95091b0c
TheLonestar1 <yura2000077@yandex.ru>
2022-08-12 18:44:35 +0700

    asd

--- PortfolioMicroservice/PortfolioMicroService.Web/Definitions/GrpcServices/GrpcDefinition.cs
+++ PortfolioMicroservice/PortfolioMicroService.Web/Definitions/GrpcServices/GrpcDefinition.cs
@@ -7,11 +7,15 @@ public class GrpcDefinition : AppDefinition
     public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddGrpc();
+       
     }
 
     public override void ConfigureApplication(WebApplication app, IWebHostEnvironment env)
     {
         app.UseRouting();
         app.UseEndpoints(endpoint => endpoint.MapGrpcService<PortfolioServices>());
+
+
+
     }
 }
