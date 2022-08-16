a387914d7c0301ee7b18ff2f15432259d258e4f7
Sergey <sstaramaly@gmail.com>
2022-08-16 13:59:52 +0300

    Add login implementation

--- BlazorClient/BlazorClient/Pages/LoginModel.cs
+++ BlazorClient/BlazorClient/Pages/LoginModel.cs
@@ -1,6 +1,9 @@
-﻿using BlazorClient.Infrastructure;
+﻿using AuthRequest;
+using BlazorClient.Infrastructure;
+using Grpc.Net.Client;
 using Microsoft.AspNetCore.Components;
 using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace BlazorClient.Pages
 {
@@ -9,6 +12,8 @@ namespace BlazorClient.Pages
         [Inject] public ILocalStorageService localStorageService { get; set; }
 
         [Inject] public NavigationManager NavigationManager { get; set; }
+
+        [Inject] IConfiguration config { get; set; }
         public LoginModel()
         {
             LoginData = new LoginViewModel();
@@ -17,12 +22,38 @@ namespace BlazorClient.Pages
         public LoginViewModel LoginData { get; set; }
         protected async Task LoginAsync()
         {
+            var address = config["FacadeBaseURL"];
+            TokenData tokenData = new TokenData();
+            try
+            {
+                var channel = GrpcChannel.ForAddress(address);
+
+                var client = new AuthService.AuthServiceClient(channel);
+                tokenData = await client.LoginAsync(new LoginData
+                {
+                    Email = LoginData.UserName,
+                    Password = LoginData.Password
+                });
+                if (tokenData == null || tokenData.Status == TokenData.Types.Status.Failed)
+                {
+                    NavigationManager.NavigateTo("/loginfailed", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                NavigationManager.NavigateTo("/loginfailed", true);
+            }
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(tokenData?.Token);
+            var exp = Convert.ToInt32(jwt.Claims.First(claim => claim.Type == "exp").Value);
+
             var token = new SecurityToken
             {
-                AccessToken = LoginData.Password,
+                AccessToken = tokenData.Token,
                 UserName = LoginData.UserName,
-                ExpiredAt = DateTime.UtcNow.AddDays(1)
+                ExpiredAt = DateTime.UtcNow.AddMilliseconds(exp)
             };
+
             await localStorageService.SetAsync(nameof(SecurityToken), token);
             NavigationManager.NavigateTo("/", true);
         }
