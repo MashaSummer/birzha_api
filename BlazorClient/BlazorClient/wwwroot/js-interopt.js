0d5ce8c99849d5ca3c9283c16add7fee6aa365c3
Sergey <sstaramaly@gmail.com>
2022-08-16 10:48:14 +0300

    add wwwroot to repo and edit gitignore

---
+++ BlazorClient/BlazorClient/wwwroot/js-interopt.js
@@ -0,0 +1,4 @@
+﻿function message(e) { alert(e); }
+function set(key, value) { localStorage.setItem(key, value); }
+function get(key) { return localStorage.getItem(key); }
+function remove(key) { return localStorage.removeItem(key); }
