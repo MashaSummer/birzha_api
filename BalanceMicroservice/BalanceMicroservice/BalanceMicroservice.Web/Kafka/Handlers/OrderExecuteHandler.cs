3b2108ca0a01f0d560e6b9f74cf9b0d6430371d5
TheLonestar1 <43960228+TheLonestar1@users.noreply.github.com>
2022-08-22 19:48:20 +0700

    Balance freezing (#41)
    * BalanceFrozen field added; OrderCreated logic renewed
    
    * Errors fix
    
    * refactoring querrys
    
    * Fix some bugs for and add frozen balance
    
    * add orderExecuteHandler & bug fix
    
    * proto type fix (double -> int32)
    
    сделал правки, но проверить не могу из за GitInfo. Подождите дальнейших правок
    
    * double -> decimal
    
    * some fix-bugs in proto and BalanceService
    
    * Fix rename and double -> int
    
    Co-authored-by: UndeadNito <113023091@mail.ru>
    Co-authored-by: UndeadNito <97860566+UndeadNito@users.noreply.github.com>

---
+++ BalanceMicroservice/BalanceMicroservice/BalanceMicroservice.Web/Kafka/Handlers/OrderExecuteHandler.cs
@@ -0,0 +1,38 @@
+﻿using BalanceMicroservice.Domain.EventsBase;
+using BalanceMicroservice.Web.MongoService;
+using Calabonga.OperationResults;
+using Confluent.Kafka;
+using OrdersEvent;
+using OrdersMicroservice.EventsBase;
+
+namespace BalanceMicroservice.Web.Kafka.Handlers;
+
+public class OrderExecuteHandler : IEventHandler<Null, OrderExecuteEvent>
+{
+    private readonly ILogger<OrderExecuteHandler> _logger;
+    private readonly MongoBalanceService _database;
+
+    public OrderExecuteHandler(MongoBalanceService mongo, ILogger<OrderExecuteHandler> logger)
+    {
+        _database = mongo;
+        _logger = logger;
+    }
+
+    public void Process(Message<Null, OrderExecuteEvent> message) => throw new NotImplementedException();
+
+    public async Task<OperationResult<bool>> ProcessAsync(Message<Null, OrderExecuteEvent> message)
+    {
+        decimal price = (decimal)message.Value.Price * message.Value.Volume / 100;
+
+        var bidUser = await _database.GetByIdAsync(new Guid(message.Value.BidInvestorId));
+        var askUser = await _database.GetByIdAsync(new Guid(message.Value.AskInvestorId));
+
+        bidUser.BalanceFrozen -= price;
+        askUser.BalanceActive += price;
+
+        await _database.UpdateAsync(bidUser);
+        await _database.UpdateAsync(askUser);
+
+        return new OperationResult<bool> { Result = true };
+    }
+}
