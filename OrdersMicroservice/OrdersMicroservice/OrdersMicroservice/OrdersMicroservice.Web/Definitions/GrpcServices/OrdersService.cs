3969a0f3bb1bea75952d360324b7794dada904fc
Artem Levin <64748654+Leo506@users.noreply.github.com>
2022-08-24 17:51:55 +0500

    Update OrdersService.cs

--- OrdersMicroservice/OrdersMicroservice/OrdersMicroservice/OrdersMicroservice.Web/Definitions/GrpcServices/OrdersService.cs
+++ OrdersMicroservice/OrdersMicroservice/OrdersMicroservice/OrdersMicroservice.Web/Definitions/GrpcServices/OrdersService.cs
@@ -49,6 +49,7 @@ public class OrdersService : Orders.OrdersService.OrdersServiceBase
             };
         }
 
+        request.OrderDetail.Price *= 100;
         await _eventProducer.ProduceAsync(null, new OrderCreatedEvent()
         {
             Order = request.OrderDetail,
