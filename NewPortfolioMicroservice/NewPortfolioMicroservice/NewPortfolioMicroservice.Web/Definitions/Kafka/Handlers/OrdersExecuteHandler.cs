c8c9b31e20f8b26487579183c3cea193f48ae0d4
Artem Levin <64748654+Leo506@users.noreply.github.com>
2022-08-24 16:17:51 +0500

    Update OrdersExecuteHandler.cs

--- NewPortfolioMicroservice/NewPortfolioMicroservice/NewPortfolioMicroservice.Web/Definitions/Kafka/Handlers/OrdersExecuteHandler.cs
+++ NewPortfolioMicroservice/NewPortfolioMicroservice/NewPortfolioMicroservice.Web/Definitions/Kafka/Handlers/OrdersExecuteHandler.cs
@@ -31,7 +31,7 @@ namespace NewPortfolioMicroservice.Definitions.Kafka.Handlers
             var askPortfolio = await _repository.GetByIdAsync(message.Value.AskInvestorId);
             var bidPortfolio = await _repository.GetByIdAsync(message.Value.BidInvestorId);
 
-            if (askPortfolio.Id == bidPortfolio.Id)
+            if (askPortfolio.Result.Id == bidPortfolio.Result.Id)
             {
                 await _repository.UpdateAsync(EncreaseBid(bidPortfolio.Result, message.Value));
                 await _repository.UpdateAsync(DecreaseAsk(bidPortfolio.Result, message.Value.ProductId, message.Value.Volume));
